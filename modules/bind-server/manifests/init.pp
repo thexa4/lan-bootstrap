6dfa928f04e8d549712563ac967153a6990ea5d8
Max Maton <max@maton.info>
2015-05-26 17:35:59 +0200

    Temporary disable reverse domain

--- modules/bind-server/manifests/init.pp
+++ modules/bind-server/manifests/init.pp
@@ -26,7 +26,7 @@ class bind-server {
 		bind::a { "$name.bolklan.nl.":
 			ensure => present,
 			zone => $domain,
-			ptr	=> true,
+			
 			zone_arpa => "1.168.192.in-addr.arpa",
 			hash_data => { owner => $ip, },
 		}
