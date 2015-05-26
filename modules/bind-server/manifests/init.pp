f728d2aa32f0036c597eefae550d0b285d98cf78
Max Maton <max@maton.info>
2015-05-26 17:20:44 +0200

    Added zone back

--- modules/bind-server/manifests/init.pp
+++ modules/bind-server/manifests/init.pp
@@ -2,6 +2,13 @@ class bind-server {
 	$domain = hiera('domain')
 	$hosts = hiera_hash('hosts')
 	
+	bind::zone { $domain:
+		zone_type		=> master,
+		zone_contact	=> "hostmaster.$domain",
+		zone_ns			=> ['ns1'],
+		zone_ttl		=> 3600,
+	}
+	
 	bind::zone { "1.168.192.in-addr.arpa":
 		zone_contact	=> "hostmaster.$domain",
 		zone_ns			=> ['ns1'],
