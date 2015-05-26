7f0e0b5c38814927d30a572cb8fd4b6d8bd62d36
Max Maton <max@maton.info>
2015-05-26 15:07:45 +0200

    More bind

--- modules/bind-server/manifests/init.pp
+++ modules/bind-server/manifests/init.pp
@@ -2,13 +2,37 @@ class bind-server {
 	$domain = hiera('domain')
 	$hosts = hiera_hash('hosts')
 	
-	bind::zone { $domain:
-		zone_contact	=> "hostmaster.$domain",
-		zone_ns			=> ['ns1'],
-		zone_serial		=> 1,
-		zone_ttl		=> 3600,
-		zone_origin		=> $domain,
+	bind::view { "all":
+		recursion	=> true,
+		zones		=> ["$domain", "1.168.192.in-addr.arpa"],
 	}
 	
+	bind::zone { $domain:
+		zone_type		=> master,
+		zone_contact	=> "hostmaster.$domain",
+		zone_ns			=> ['ns1'],
+		zone_ttl		=> 3600,
+	}
 	
+	bind::zone { "1.168.192.in-addr.arpa":
+		zone_type		=> master,
+		zone_contact	=> "hostmaster.$domain",
+		zone_ns			=> ["ns1.$domain."],
+		zone_ttl		=> 3600.
+	}
+	
+	define hieraHost {
+		$hosts = hiera_hash('hosts')
+		$ip = $hosts[$name]
+		
+		resource_record { "$name.bolklan.nl.":
+			ensure => present,
+			record => "$name.bolklan.nl",
+			type => "A",
+			data => $ip,
+		}
+	}
+	
+	$keys = keys($hosts)
+	hieraHost { $keys: }
 }
