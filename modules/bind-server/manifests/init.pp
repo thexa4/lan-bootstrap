ac6e4920be80fcdfe73d28d19a7d421282311bab
Max Maton <max@maton.info>
2015-05-26 20:14:32 +0200

    Reload bind

--- modules/bind-server/manifests/init.pp
+++ modules/bind-server/manifests/init.pp
@@ -39,6 +39,7 @@ class bind-server {
 		line => '	allow-recursion { 192.168.1.0/24; };',
 		after => 'options {',
 		require => Bind::Zone[$domain],
+		notify => Service['bind9'],
 	}
 	
 	file_line { '/etc/bind/named.conf.options forwarders':
@@ -46,6 +47,7 @@ class bind-server {
 		line => '	forwarders { 8.8.8.8; 8.8.4.4; };',
 		after => 'options {',
 		require => Bind::Zone[$domain],
+		notify => Service['bind9'],
 	}
 	
 	$keys = keys($hosts)
