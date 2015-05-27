0ffe09bc3c382b2cd9dc3a4863bce561f09ebb3c
Max Maton <max@maton.info>
2015-05-27 13:47:53 +0200

    Add site config

--- modules/cache/manifests/init.pp
+++ modules/cache/manifests/init.pp
@@ -7,4 +7,19 @@ class cache {
 		command => "/usr/bin/wget -qO- http://blog.multiplay.co.uk/dropzone/lancache-386.tgz | gunzip | /bin/tar -xC /etc/nginx/ lancache",
 		unless => "/usr/bin/test -d /etc/nginx/lancache",
 	}
+	
+	file { "/etc/nginx/sites-available/steam":
+		ensure => present,
+		source => "puppet:///modules/cache/steam",
+	}
+	
+	file { "/etc/nginx/sites-enabled/steam":
+		ensure => link,
+		target => "/etc/nginx/sites-available/steam",
+		notify => Service["nginx"],
+	}
+	
+	service { "nginx":
+		ensure => running,
+	}
 }
