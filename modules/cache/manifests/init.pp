4ce6a4a5f2ca30d22954408055b2251a3cda92c2
Max Maton <max@maton.info>
2015-05-27 13:45:00 +0200

    Add unzip step

--- modules/cache/manifests/init.pp
+++ modules/cache/manifests/init.pp
@@ -4,7 +4,7 @@ class cache {
 	}
 	
 	exec { "download nginx-cache config":
-		command => "/usr/bin/wget -qO- http://blog.multiplay.co.uk/dropzone/lancache-386.tgz | /bin/tar -xC /etc/nginx/ lancache",
+		command => "/usr/bin/wget -qO- http://blog.multiplay.co.uk/dropzone/lancache-386.tgz | gunzip | /bin/tar -xC /etc/nginx/ lancache",
 		unless => "/usr/bin/test -d /etc/nginx/lancache",
 	}
 }
