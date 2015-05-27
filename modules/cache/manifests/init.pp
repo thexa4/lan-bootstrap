4b05f326cd132ede8e6bc9eceb4de4fcc467431f
Max Maton <max@maton.info>
2015-05-27 14:01:23 +0200

    Use upstream nginx

--- modules/cache/manifests/init.pp
+++ modules/cache/manifests/init.pp
@@ -1,6 +1,7 @@
 class cache {
 	package { "nginx":
 		ensure => present,
+		require => [ File["/etc/apt/sources.list.d/nginx.conf"], Exec["refresh apt-get nginx"] ],
 	}
 	
 	exec { "download nginx-cache config":
@@ -21,5 +22,25 @@ class cache {
 	
 	service { "nginx":
 		ensure => running,
+	}
+	
+	file { "/etc/nginx/nginx.key":
+		ensure => file,
+		source => "puppet:///modules/cache/nginx.key",
+	}
+	
+	exec { "install nginx key":
+		command => "/usr/bin/apt-key add /etc/nginx/nginx.key",
+		unless => "/usr/bin/apt-key list | /bin/grep -q Nginx",
+		require => File["/etc/nginx/nginx.key"],
+	}
+	
+	file { "/etc/apt/sources.list.d/nginx.list":
+		ensure => file,
+		source => "puppet:///modules/cache/nginx.list",
+	}
+	
+	Exec { "refresh apt-get nginx":
+		command => "/usr/bin/apt-get update",
 	}
 }
