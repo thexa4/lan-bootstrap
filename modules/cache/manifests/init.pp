0e17cb469db58e0970a88ce76d4577ab6acbc181
Max Maton <max@maton.info>
2015-05-27 14:02:31 +0200

    Fix dependencies

--- modules/cache/manifests/init.pp
+++ modules/cache/manifests/init.pp
@@ -7,21 +7,25 @@ class cache {
 	exec { "download nginx-cache config":
 		command => "/usr/bin/wget -qO- http://blog.multiplay.co.uk/dropzone/lancache-386.tgz | gunzip | /bin/tar -xC /etc/nginx/ lancache",
 		unless => "/usr/bin/test -d /etc/nginx/lancache",
+		require => Package["nginx"],
 	}
 	
 	file { "/etc/nginx/sites-available/steam":
 		ensure => present,
 		source => "puppet:///modules/cache/steam",
+		require => Package["nginx"],
 	}
 	
 	file { "/etc/nginx/sites-enabled/steam":
 		ensure => link,
 		target => "/etc/nginx/sites-available/steam",
 		notify => Service["nginx"],
+		require => Package["nginx"],
 	}
 	
 	service { "nginx":
 		ensure => running,
+		require => Package["nginx"],
 	}
 	
 	file { "/etc/nginx/nginx.key":
