47fef0deac7c80b5953124e1ef944b123d04f310
Max Maton <max@maton.info>
2015-05-27 13:34:19 +0200

    Add cache behavior

--- modules/cache/manifests/init.pp
+++ modules/cache/manifests/init.pp
@@ -2,4 +2,9 @@ class cache {
 	package { "nginx":
 		ensure => present,
 	}
+	
+	exec { "download nginx-cache config":
+		command => "/usr/bin/wget -qO- http://blog.multiplay.co.uk/dropzone/lancache-386.tgz | /bin/tar -xC /etc/nginx/ lancache",
+		unless => "/usr/bin/test -d /etc/nginx/lancache",
+	}
 }
