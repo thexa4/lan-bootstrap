5b3fd6900672a633fd0a59d855337227d5a5b0a8
Max Maton <max@maton.info>
2015-05-29 15:35:14 +0200

    Invert test

--- modules/openttd/manifests/init.pp
+++ modules/openttd/manifests/init.pp
@@ -2,7 +2,7 @@ class openttd {
 	
 	exec { "install openttd":
 		command => "wget -qO- http://binaries.openttd.org/releases/1.5.0/openttd-1.5.0-linux-generic-i686.tar.gz | gunzip | tar -xC /opt/",
-		onlyif => "[ -d /opt/openttd/ ]",
+		onlyif => "[ ! -d /opt/openttd/ ]",
 	}
 	
 	package { "tmux":
