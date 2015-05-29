d720cf59779bb25ecb09a4facf834d2ffb515520
Max Maton <max@maton.info>
2015-05-30 00:34:31 +0200

    Fix download url

--- modules/trackmania/manifests/init.pp
+++ modules/trackmania/manifests/init.pp
@@ -12,8 +12,8 @@ class trackmania {
 	}
 	
 	exec { "extract dedicated trackmania server":
-		command => "wget -qO- http://kheops.unice.fr/Slig/tm/dedicated/TmDedicatedServer_2006-05-30.zip > /opt/tmdedicated.zip && unzip /opt/tmdedicated.zip",
-		unless => "[ -f /opt/tmdedicated.zip ]",
+		command => "wget -qO- http://slig.free.fr/TM/dedicated/TmDedicatedServer_2006-05-30.zip > /opt/tmdedicated.zip && unzip /opt/tmdedicated.zip",
+		unless => "[ -d /opt/TmDedicatedServer ]",
 		require => Package["unzip"],
 	}
 }
