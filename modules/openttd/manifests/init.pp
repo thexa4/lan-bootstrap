4fef9f5d66cd74716f329a1233245a151a0e2de4
Max Maton <max@maton.info>
2015-05-29 15:19:07 +0200

    Add startup script

--- modules/openttd/manifests/init.pp
+++ modules/openttd/manifests/init.pp
@@ -3,5 +3,12 @@ class openttd {
 		ensure => present,
 	}
 	
+	package { "tmux":
+		ensure => present,
+	}
 	
+	file { "/etc/rc.local":
+		ensure => present,
+		source => "puppet:///modules/openttd/rc.local",
+	}
 }
