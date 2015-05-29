581ff62cc853d2311c5b75572c65bcc329acada1
Max Maton <max@maton.info>
2015-05-29 15:49:52 +0200

    Add opengfx

--- modules/openttd/manifests/init.pp
+++ modules/openttd/manifests/init.pp
@@ -5,6 +5,16 @@ class openttd {
 		onlyif => "[ ! -d /opt/openttd-1.5.0-linux-generic-i686/ ]",
 	}
 	
+	exec { "download openttd-opengfx":
+		command => "wget -qO- http://binaries.openttd.org/extra/opengfx/0.5.2/opengfx-0.5.2-all.zip > /opt/opengfx.zip && unzip /opt/opengfx.zip && rm /opt/opengfx.zip",
+		onlyif => "[ ! -d /opt/opengfx ]",
+		require => Package["unzip"],
+	}
+	
+	package { "unzip":
+		ensure => present,
+	}
+	
 	file { "/opt/openttd":
 		ensure => link,
 		target => "/opt/openttd-1.5.0-linux-generic-i686",
