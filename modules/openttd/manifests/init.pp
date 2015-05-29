bfe06af74a9721f4b1ab5489f054563a94d68a4b
Max Maton <max@maton.info>
2015-05-29 15:30:57 +0200

    Fetch from source

--- modules/openttd/manifests/init.pp
+++ modules/openttd/manifests/init.pp
@@ -1,6 +1,8 @@
 class openttd {
-	package { "openttd":
-		ensure => present,
+	
+	exec { "install openttd":
+		command => "wget -qO- http://binaries.openttd.org/releases/1.5.0/openttd-1.5.0-linux-generic-i686.tar.gz | gunzip | tar -xC /opt/",
+		onlyif => "[ -d /opt/openttd/ ]",
 	}
 	
 	package { "tmux":
