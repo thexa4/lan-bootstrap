e7ed6836328c3fb439dc84a013ae7b3ff8fe6399
Max Maton <max@maton.info>
2015-05-30 20:05:23 +0200

    Fix syntax errors

--- modules/empires/manifests/init.pp
+++ modules/empires/manifests/init.pp
@@ -1,4 +1,4 @@
-class empires {
+class empires($username = "", $password = "", $verification = "") {
 	package { "wine":
 		ensure => present,
 	}
@@ -11,15 +11,25 @@ class empires {
 		ensure => directory,
 	}
 	
+	file { "/opt/empires":
+		ensure => directory,
+	}
+	
 	exec { "download updatetool.rar":
 		command => "wget -qO- http://didrole.com/UpdateTool/UpdateTool-0.3.rar > /opt/updatetool/updatetool.rar",
 		unless => "[ -f /opt/updatetool/updatetool.rar ]",
-		requires => File["/opt/updatetool"],
+		require => File["/opt/updatetool"],
 	}
 	
 	exec { "extract updatetool.rar":
 		command => "(cd /opt/updatetool/ && rar e updatetool.rar)",
 		unless => "[ -f /opt/updatetool/UpdateTool.exe ]",
-		requires => [ Package["unrar"], Exec["download updatetool.rar"] ],
-	}	
+		require => [ Package["unrar"], Exec["download updatetool.rar"] ],
+	}
+	
+	exec { "fetch empires":
+		command => "/opt/updatetool/UpdateTool.exe -command install -game 17740 -dir /opt/empires/ -username $username -password $password -verify_all -steam_guard_code $verify",
+		unless => "/bin/false",
+		require => [ Exec["extract updatetool.rar"], Package["wine"] ],
+	}
 }
