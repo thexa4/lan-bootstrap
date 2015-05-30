caf550f9b01f2895ec24b6cbb26c335049529ea4
Max Maton <max@maton.info>
2015-05-30 20:00:33 +0200

    Extract stuff

--- modules/empires/manifests/init.pp
+++ modules/empires/manifests/init.pp
@@ -7,8 +7,19 @@ class empires {
 		ensure => present,
 	}
 	
-	exec { "download updatetool.rar":
-		command => "wget -qO- http://didrole.com/UpdateTool/UpdateTool-0.3.rar > /opt/updatetool.rar",
-		unless => "[ -f /opt/updatetool.rar ]",
+	file { "/opt/updatetool":
+		ensure => directory,
 	}
+	
+	exec { "download updatetool.rar":
+		command => "wget -qO- http://didrole.com/UpdateTool/UpdateTool-0.3.rar > /opt/updatetool/updatetool.rar",
+		unless => "[ -f /opt/updatetool/updatetool.rar ]",
+		requires => File["/opt/updatetool"],
+	}
+	
+	exec { "extract updatetool.rar":
+		command => "(cd /opt/updatetool/ && rar e updatetool.rar)",
+		unless => "[ -f /opt/updatetool/UpdateTool.exe ]",
+		requires => [ Package["unrar"], Exec["download updatetool.rar"] ],
+	}	
 }
