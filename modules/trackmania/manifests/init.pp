7507d6e8d3295764cbd10d2a30a5f873a07c104c
Jakob Buis <jakob@jakobbuis.nl>
2016-05-14 20:37:10 +0200

    use a newer version of the dedicated TrackmaniaServer, added needed parameter to rc.local

--- modules/trackmania/manifests/init.pp
+++ modules/trackmania/manifests/init.pp
@@ -12,7 +12,7 @@ class trackmania {
 	}
 	
 	exec { "extract dedicated trackmania server":
-		command => "wget -qO- http://slig.free.fr/TM/dedicated/TmDedicatedServer_2006-05-30.zip > /opt/tmdedicated.zip && unzip /opt/tmdedicated.zip",
+		command => "wget -qO- http://files2.trackmaniaforever.com/TrackmaniaSErver_2011-02-21.zip > /opt/tmdedicated.zip && unzip /opt/tmdedicated.zip",
 		unless => "[ -d /opt/TmDedicatedServer ]",
 		require => Package["unzip"],
 	}
