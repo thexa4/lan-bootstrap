e7b67c04e3dafc60b8c9fbfd2e28c2a54b534b10
Max Maton <max@maton.info>
2016-05-15 15:10:15 +0200

    Fix test location

--- modules/factorio/manifests/init.pp
+++ modules/factorio/manifests/init.pp
@@ -1,7 +1,7 @@
 class factorio {
 	exec { "download-factorio-tar":
 		command => '/usr/bin/wget -qO- http://www.factorio.com/get-download/0.12.33/headless/linux64 > /opt/factorio.tgz',
-		unless => '/bin/test -f /opt/factorio.tgz ]',
+		unless => '/usr/bin/test -f /opt/factorio.tgz ]',
 	}
 
 	package { "tmux":
